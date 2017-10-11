435ab140a6019a14e397989026b91eb426842216
Manjot Sidhu <manjot.gni@gmail.com>
2017-10-11 13:25:27 +0530

    a106: New Patches

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -3,6 +3,8 @@ cd ../../../..
 cd system/core
 patch -p1 < ../../device/micromax/a106/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
 patch -p1 < ../../device/micromax/a106/patches/0020-healthd_batteryVoltage.patch
+patch -p1 < ../../device/micromax/a106/patches/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
+patch -p1 < ../../device/micromax/a106/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
 cd ../..
 cd bionic
 patch -p1 < ../device/micromax/a106/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
