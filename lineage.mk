b74b4cbe0d8b3d77d233d056f7635a52ecbe8988
Manjot Sidhu <manjot.gni@gmail.com>
2017-11-11 18:42:34 +0530

    lineage: switch to lineage vendor

--- lineage.mk
+++ lineage.mk
@@ -12,7 +12,7 @@ LOCAL_PATH := device/micromax/a106
 # Common CM stuff
 CM_BUILD := a106
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=7.1.2/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="a106-user 7.1.2 MRA58M 2280749 release-keys"
 
