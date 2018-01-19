1fd5aad1e0b7b3b96e02ed047f343d550ccf9e9c
Manjot Sidhu <manjot.gni@gmail.com>
2018-01-19 12:07:23 +0530

    ramdisl: Enable Zram

--- lineage_a106.mk
+++ lineage_a106.mk
@@ -83,8 +83,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/root/ueventd.sprout.rc:root/ueventd.sprout.rc \
     $(LOCAL_PATH)/rootdir/root/init.sprout.usb.rc:root/init.sprout.usb.rc \
     $(LOCAL_PATH)/rootdir/root/init.zeta0y_core.rc:root/init.zeta0y_core.rc \
-    $(LOCAL_PATH)/rootdir/root/fstab.sprout:root/fstab.sprout \
-    $(LOCAL_PATH)/rootdir/root/enableswap.sh:root/enableswap.sh
+    $(LOCAL_PATH)/rootdir/root/fstab.sprout:root/fstab.sprout
 	
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/root/twrp.fstab:recovery/root/etc/twrp.fstab
