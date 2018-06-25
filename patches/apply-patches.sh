a9e49d045227e9ed77362dfcc9b98f3cb3de42c5
Manjot Sidhu <manjot.gni@gmail.com>
2018-06-25 11:11:17 +0530

    patches: add earlysuspend patch

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -2,6 +2,7 @@
 cd ../../../..
 cd system/core
 patch -p1 < ../../device/micromax/a106/patches/system_core.patch
-cd ../..
-cd hardware/interfaces
-patch -p1 < ../../device/micromax/a106/patches/hardware_interfaces.patch
+patch -p1 < ../../device/micromax/a106/patches/mkbootimg1.patch
+patch -p1 < ../../device/micromax/a106/patches/mkbootimg2.patch
+patch -p1 < ../../device/micromax/a106/patches/mkbootimg3.patch
+patch -p1 < ../../device/micromax/a106/patches/earlysuspend.patch
