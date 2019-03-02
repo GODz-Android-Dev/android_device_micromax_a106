fe7b0e8c14c6cb7abed6d16bbf52b97037a3ec1f
Badshahpk <Piyushkulkarni48@gmail.com>
2019-03-02 09:29:33 +0530

    Delete apply-patches.sh

--- patches/apply-patches.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ../../../..
-cd system/core
-patch -p1 < ../../device/micromax/a106/patches/system_core.patch
-patch -p1 < ../../device/micromax/a106/patches/mkbootimg1.patch
-patch -p1 < ../../device/micromax/a106/patches/mkbootimg2.patch
-patch -p1 < ../../device/micromax/a106/patches/mkbootimg3.patch
-patch -p1 < ../../device/micromax/a106/patches/earlysuspend.patch
-cd ../../
